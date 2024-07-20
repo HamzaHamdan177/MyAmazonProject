91cb2b19887a459427bed95f153aee2fb22d9805
hamza <babamamadw2@gmail.com>
2024-07-20 10:02:50 +0300

    convert product into classes

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -44,9 +44,7 @@ export function renderOrderSummary() {
           <img class="product-image" src="${matchingProduct.image}">
           <div class="cart-item-details">
             <div class="product-name">${matchingProduct.name}</div>
-            <div class="product-price">$${formatCurrency(
-              matchingProduct.priceCents
-            )}</div>
+            <div class="product-price">$${matchingProduct.getPrice()}</div>
             <div class="product-quantity">
               <span>
                 Quantity: <span class="quantity-label">${
